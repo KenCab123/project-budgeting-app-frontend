cd354ad5c2500d82a32ceae9c86054a35265b7cc
KenCab123 <KennethCabral@pursuit.org>
2024-02-28 14:29:52 -0500

    implemented delete button and functionthat fetches, sends an alert, and is directed to the home page

--- src/TransactionDetails.jsx
+++ src/TransactionDetails.jsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 
-export const TransactionDetails = () => {
+export const TransactionDetails = ({setAllTransactions}) => {
+    const navigate = useNavigate()
     const {id} = useParams()
     const [transactionDetail, setTransactionDetail] = useState()
+
+    const handleDelete = (id) => {
+        const options = {
+            method: "DELETE"
+        }
+
+        fetch(`http://localhost:3333/api/transactions/${id}`, options).then(res => res.json()).then(data => setAllTransactions(data.transactions))
+        alert(`Transaction Deleted!`)
+        navigate('/')
+    }
 
     useEffect(() => {
         fetch(`http://localhost:3333/api/transactions/${id}`).then(res => res.json()).then(data => setTransactionDetail(data.transaction))
@@ -22,6 +33,7 @@ export const TransactionDetails = () => {
         <Link to={`/edit/${transactionDetail.id}`}>
             <button>Edit</button>
         </Link>
+            <button onClick={() => handleDelete(transactionDetail.id)}>Delete</button>
         <Link to='/'>
             <button>Home</button>
         </Link>
