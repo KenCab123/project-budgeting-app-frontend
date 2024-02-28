4394fb90340ad791219400f7996452c74c70855a
KenCab123 <KennethCabral@pursuit.org>
2024-02-28 14:44:23 -0500

    added color conditional on the total bank account price

--- src/Transactions.jsx
+++ src/Transactions.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const Transactions = ({allTransactions}) => {
-    // const totalPrice = 
+
+    const handleTotalPrice = (amount) => {
+        if(amount < 0) return <h2>Total Price: <span style={{color: 'red'}}>${amount}</span></h2>
+        if(amount < 100 && amount > 0) return <h2>Total Price: <span style={{color: 'yellow'}}>${amount}</span></h2>
+        return <h2>Total Price: <span style={{color: 'green'}}>${amount}</span></h2>
+    }
+    
   return (
     <div>
-        <h2>Total Price: ${allTransactions.reduce((acc, curr) => acc + curr.amount,0)}</h2>
-        {/* <h2>{allTransactions.map(t => console.log(t.amount))}</h2> */}
+        {/* <h2>Total Price: ${allTransactions.reduce((acc, curr) => acc + curr.amount,0)}</h2> */}
+        {handleTotalPrice(allTransactions.reduce((acc, curr) => acc + curr.amount,0))}
     {allTransactions.map(({id, item_name, amount, date, from, category}) => {
         return (
             <Link to={`/${id}`} key={id}>
