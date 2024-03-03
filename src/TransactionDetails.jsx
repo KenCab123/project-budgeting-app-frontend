e734e116df9fa29ed5bc5b4fc876ac89b9237315
KenCab123 <KennethCabral@pursuit.org>
2024-03-03 13:31:11 -0500

    added css style for TransactionsDetails

--- src/TransactionDetails.jsx
+++ src/TransactionDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, Link, useNavigate } from 'react-router-dom'
+import "./TransactionDetails.css"
 
 export const TransactionDetails = ({setAllTransactions, handleDate}) => {
     const navigate = useNavigate()
@@ -23,13 +24,16 @@ export const TransactionDetails = ({setAllTransactions, handleDate}) => {
     if(!transactionDetail) return null
 
   return (
-    <div>
+    <div className='details'>
         <h1>Transaction Details</h1>
+        <div>
         <h2>{transactionDetail.item_name[0].toUpperCase() + transactionDetail.item_name.slice(1)}</h2>
         <p>Amount: ${transactionDetail.amount}</p>
         <p>Date: {handleDate(transactionDetail.date)}</p>
         <p>From: {transactionDetail.from}</p>
         <p>Category: {transactionDetail.category}</p>
+        </div>
+        <div className='btns'>
         <Link to={`/edit/${transactionDetail.id}`}>
             <button>Edit</button>
         </Link>
@@ -37,6 +41,7 @@ export const TransactionDetails = ({setAllTransactions, handleDate}) => {
         <Link to='/'>
             <button>Home</button>
         </Link>
+        </div>
     </div>
   )
 }
