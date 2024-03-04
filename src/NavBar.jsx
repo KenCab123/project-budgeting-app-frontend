7d30e8fd7bf09210f8e039d349cb0bbcbfd0d1b2
KenCab123 <KennethCabral@pursuit.org>
2024-03-04 11:38:51 -0500

    cleaned up NavBar.jsx and NavBar.css

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,27 +1,26 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import "./NavBar.css"
 
 
-export const NavBar = ({allTransactions, setPrice, price, handleTotalPrice, dataLoaded}) => {
+export const NavBar = ({allTransactions, setPrice, price, handleTotalPrice}) => {
 
   useEffect(() => {
 
     if (allTransactions.length > 0) {
       setPrice(allTransactions.reduce((acc, curr) => acc + curr.amount, 0));
     } 
+
   }, [allTransactions]);
-
-
-  
 
   return (
     <div className='nav'>
     <h1>Budgeting App</h1>
     
     <Link to='/new'>
-    <button>Create</button>
+      <button>Create</button>
     </Link>
+    
     {handleTotalPrice(price)}
     </div>
   )
